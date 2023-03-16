6c6cb55ba30536b44be396e617cfd4a9efe33791
FloBeni <florian.benitiere@gmail.com>
2023-03-16 14:13:27 +0100

    update

--- values_in_text_paper_generator.R
+++ values_in_text_paper_generator.R
@@ -7,7 +7,7 @@
   data_1 = read.delim("data/Data1_supp.tab",comment.char = "#")
   data_1 = data_1[data_1$nb_busco > .8 * 978,]
   data_1 = data_1[data_1$CoverageBuscoExon > 196 ,]
-  print(paste("To test this hypothesis, we performed a meta-analysis of ",
+  print(paste("To test this hypothesis, we analyzed ",
               sum(data_1$nb_rnaseq)," transcriptome sequencing samples to quantify AS in ",nrow(data_1),
               " metazoan species spanning a wide range of Ne values",sep=""))
   
@@ -45,10 +45,14 @@
               round(sum(data_1$nb_rnaseq)/nrow(data_1))," per species on average).",sep=""))
   
   print(paste("among BUSCO genes ranges from ",round(min(data_1$analyzable_intron_busco),1)," to ",
-              round(max(data_1$analyzable_intron_busco),1)," which represents "
+              round(max(data_1$analyzable_intron_busco),1)," (which represents "
               ,round(min(data_1$analyzable_intron_busco/data_1$annotated_intron_busco*100),1),"% to ",round(max(data_1$analyzable_intron_busco/data_1$annotated_intron_busco*100),1),
-              "% (median=",round(median(data_1$analyzable_intron_busco/data_1$annotated_intron_busco*100),1),"%;",sep=""))
+              "% of their introns;",sep=""))
   
+  
+  data_10 = read.delim("data/Data10_supp.tab",comment.char = "#")
+  data_10 = data_10[data_10$species %in% data_1$species,]
+  print(paste("very homogenous in terms of sequencing technology (",round(sum(data_10$Platform == "ILLUMINA") / nrow(data_10)*100,0),"% of RNA-seq samples sequenced with Illumina platforms)",sep=""))
   
   ######
   print("Proxies for the effective population size (Ne)")
@@ -68,7 +72,7 @@
   ######
   print("Alternative splicing rates are negatively correlated with Ne proxies")
   print(paste("In vertebrates, each BUSCO gene contains on average ",round(mean(data_1[data_1$clade %in% c("Mammalia", "Crocodylia","Aves", "Chondrichthyes" ),]$mean_intron_per_gene_busco),1),
-              " introns. The intron density is more variable among insect clades, ranging from ",round(mean(data_1[data_1$clade %in% c("Diptera" ),]$mean_intron_per_gene_busco),1),
+              " introns (Supplementary Tab. 1). The intron density is more variable among insect clades, ranging from ",round(mean(data_1[data_1$clade %in% c("Diptera" ),]$mean_intron_per_gene_busco),1),
               " introns per BUSCO gene in Diptera to ",round(mean(data_1[data_1$clade %in% c("Blattodea" ),]$mean_intron_per_gene_busco),1)," in Blattodea.",sep=""))
   
   
@@ -82,21 +86,21 @@
               round(mean(data_1$splsite_atac_minor_busco)*100,1), "% AT/AC).",sep=""))
   
   
-  print(paste("The proportion of major introns for which AS has been detected (i.e. with N2>0) ranges from ",
+  print(paste("The proportion of major introns for which AS has been detected (i.e. with Na > 0) ranges from ",
               round(min(data_1$prop_major_sv_busco)*100,1),"% to ",round(max(data_1$prop_major_sv_busco)*100,1),"%",sep=""))
   
   
   svr_busco = tapply(data_1$mean_as_busco,data_1$species,sum)
   
-  print(paste("The average AS rate for BUSCO genes varies by a factor of ",round(max(svr_busco) / min(svr_busco)) ,", from ",
+  print(paste("The average AS rate for BUSCO genes varies by a factor of ",round(max(svr_busco) / min(svr_busco)) ," among species, from ",
               round(svr_busco["Drosophila_grimshawi"],1),"% in Drosophila grimshawi (Diptera) to ",round(svr_busco["Megachile_rotundata"],1),"% in Megachile rotundata (Hymenoptera) (",
               round(svr_busco["Homo_sapiens"],1),"% in humans).",sep=""))
   
-  svr=read.table("data/Data4_supp.tab",h=T,stringsAsFactors=F)
+  data_4 = read.delim("data/Data4_supp.tab",comment.char = "#")
+  data_4 = data_4[data_4$species != "Dendroctonus_ponderosae",]
+  data_4 = data_4[which(data_4$organs %in% c("Brain", "Cerebellum", "Heart", "Kidney","Head", "Liver", "Ovary", "Testis")),] ## pour enlèver KidneyTestis et WholeBrain, pour lesquels on n'avait pas toutes les espèces.
   
-  svr = svr[which(svr$organs %in% c("Brain", "Cerebellum", "Heart", "Kidney", "Liver", "Ovary", "Testis")),] ## pour enlèver KidneyTestis et WholeBrain, pour lesquels on n'avait pas toutes les espèces.
-  
-  a = anova(lm(svr$SVR~(svr$species+svr$organs)))
+  a = anova(lm(data_4$SVR~(data_4$species+data_4$organs)))
   output_anova = 100*a[["Sum Sq"]]/sum(a[["Sum Sq"]])
   
   print(paste( "Specifically, in an ANOVA analysis performed on the average AS rate across BUSCO gene introns, with the species and the organ of origin as explanatory variables, the species factor explained ",
@@ -107,14 +111,19 @@
   print("Functional vs. non-functional alternative splicing")
   
   print(paste("Of note, the subset of rare SVs represents the vast majority of the SV repertoire (from ",round(min(data_1$prop_rare_sv_protein_coding),1),
-              "% to ",round(max(data_1$prop_rare_sv_protein_coding),1),"% depending on the species;",sep=""))
+              "% to ",round(max(data_1$prop_rare_sv_protein_coding),1),"% depending on the species; Supplementary Tab. 1).",sep=""))
+  
+  
+  #########
+  print("Investigating selective pressures on minor splice sites")
+  print(paste("located within the major intron show a weak but significant SNP deficit relative to corresponding control sites (p=xx19), ",sep=""))
   
   
   #########
   print("The splicing rate of rare SVs is negatively correlated with gene expression levels")
   
-  print(paste("The selected subset represents ",round(min(data_1$prop_major_nt_sup100 *100),1),"% to ",
-              round(max(data_1$prop_major_nt_sup100 *100),1),"% of introns of each species (median=",round(mean(data_1$prop_major_nt_sup100 *100),1),"%).",sep=""))
+  print(paste("The selected subset represents ",round(min(data_1$prop_major_nt_sup100 * 100),1),"% to ",
+              round(max(data_1$prop_major_nt_sup100 * 100),1),"% of introns of each species (median=",round(mean(data_1$prop_major_nt_sup100 * 100),1),"%).",sep=""))
   
   print(paste("AS rate and gene expression level in ",sum(data_1$cor_as_fpkm_low_as < 0)," out of the ",nrow(data_1 ),
               " species (significant with p < 0.05, in ", sum(data_1$cor_as_fpkm_low_as < 0 & data_1$pval_cor_as_fpkm_low_as < 0.05),"/",nrow(data_1 )," species;",sep=""))
@@ -129,19 +138,27 @@
   
   print(paste("publicly available RNA-seq data across a large set of ",nrow(data_1)," species",sep=""))
   
-  print(paste("With this setting, on average ",round(mean(data_1$prop_analyzable_busco*100),1),"% of BUSCO annotated introns could be analyzed in each species.",sep=""))
+  print(paste("With this setting, on average ",round(mean(data_1$analyzable_intron_busco/data_1$annotated_intron_busco*100),1),"% of BUSCO annotated introns could be analyzed in each species.",sep=""))
   
-  print(paste("We observed a ",round(max(svr_busco) / min(svr_busco)) ,"-fold variation in the average AS rate of BUSCO introns across species (Fig. 2B).",sep=""))
+  print(paste("We observed a ",round(max(svr_busco) / min(svr_busco)) ,"-fold variation in the average AS rate of BUSCO introns across species (Fig. 3A).",sep=""))
   
   print(paste("Rare SVs represent the vast majority of the repertoire of splicing isoforms detected in a given transcriptome (from ",
               round(min(data_1$prop_rare_sv_protein_coding),1),
               "% to ",
               round(max(data_1$prop_rare_sv_protein_coding),1),
-              "% according to the species;",sep=""))
+              "% according to the species; Supplementary Tab. 1).",sep=""))
   
+  
+  
+  
+  print(paste("In agreement with previous work, we observed that AS rates tend to be high in vertebrates (average=",
+              round( mean(data_1[data_1$clade %in% c("Mammalia", "Crocodylia","Aves", "Chondrichthyes" ),]$mean_as_busco),1),
+              "%). and notably in primates (average=",
+              round( mean(data_1[data_1$species %in% c("Homo_sapiens","Macaca_mulatta" ),]$mean_as_busco),1)
+              ,"%).",sep=""))
   
   print(paste("Given that intron density varies widely across clades (from ",
               round(mean(data_1[data_1$clade %in% c("Diptera" ),]$mean_intron_per_gene_busco),1),
               " introns per gene in diptera to ",round(mean(data_1[data_1$clade %in% c("Mammalia", "Crocodylia","Aves", "Chondrichthyes" ),]$mean_intron_per_gene_busco),1),
-              " introns per gene in vertebrates),",sep=""))
+              " introns per gene in vertebrates; Supplementary Tab. 1),",sep=""))
 }
